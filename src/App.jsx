1fc02b2a44a01ff6cb8bf2277ebaf78c01e4385f
Aryan Jain <aryanofficialjain@gmail.com>
2024-03-16 23:58:38 +0530

    all is donr but remain checkbox fuctionaity

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,54 @@ const App = () => {
 
   const [TaskInput, setTaskInput] = useState();
   const [TaskContainer, setTaskContainer] = useState([]);
+  const [iteminput, setiteminput] = useState();
   const addtask = () => {
-    if(TaskInput.trim() !== ''){
+    if (TaskInput.trim() !== '') {
       setTaskContainer([...TaskContainer, TaskInput]);
-      setTaskInput('');  
+      setTaskInput('');
     }
-     
-  }
 
-  const ondelete = (idd) => {
-    const newlist = TaskContainer.filter((item, id)=> id !== idd);
+  }
+  const [toggle, settoggle] = useState(true);
+  const edit = (iid) => {
+    settoggle(!toggle);
+    const newlist = TaskContainer.map((item, id) => id === iid ? iteminput : item);
     setTaskContainer(newlist);
 
 
   }
 
-  const edit = (iid, newitem) => {
-    const newlist = TaskContainer.map((item, id)=> id === iid ? newitem : item)
-    setTaskContainer(newlist);
+  const ondel = (iid) => {
+    const newcontainer = TaskContainer.filter((item, id) => id !== iid);
+    setTaskContainer(newcontainer);
+
 
   }
 
+// implemet the check list 
 
   return (
-    <div>
+    <div className='bg-black text-white flex items-center justify-center min-h-screen text-center '>
       <div>
         <h1>TODO List</h1>
-        
+
         <div>
-          <input type="text" placeholder='Task Here' value={TaskInput} onChange={(e) => setTaskInput(e.target.value)}  />
-          <button onClick={addtask}>add</button>  
+          <input type="text" placeholder='Task Here' value={TaskInput} onChange={(e) => setTaskInput(e.target.value)} className='text-black' />
+          <button onClick={addtask}>add</button>
         </div>
-      
-      {/*container list map  */}
-      <div>
-        {TaskContainer.map((item, id)=> (
-          <div key={id} className='flex gap-3'>
-            <input type="text" value={item} onChange={(e) => edit(id, e.target.value)} />
-            <button onClick={() => ondelete(id)}>delete</button>
-          </div>
-        ))}
-      </div>
-      
+
+        {/*container list map  */}
+        <div>
+          {TaskContainer.map((item, id) => (
+            <div key={id} className='flex gap-3'>
+              {/* <input type="checkbox" onChange={()=> check(id)} checked={checkkar[id]} name="" id="" /> */}
+              <input type="text" value={item} readOnly={toggle} className={checkkar ? ('text-red-500'): ('text-black')} onChange={(e) => setiteminput(e.target.value)} />
+              <button onClick={() => ondel(id)}>Delete</button>
+              <button onClick={() => edit(id)}>{toggle===false ? 'Save' : 'Edit'}</button>
+            </div>
+          ))}
+        </div>
+
       </div>
     </div>
   )
