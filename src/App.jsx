1d59c2cf10602ae4970eba14c1dd3dd615165ba8
Aryan Jain <aryanofficialjain@gmail.com>
2024-03-17 17:43:24 +0530

    make all the fucntion correct but not have checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
 
-  const [TaskInput, setTaskInput] = useState();
+  const [TaskInput, setTaskInput] = useState('');
   const [TaskContainer, setTaskContainer] = useState([]);
-  const [iteminput, setiteminput] = useState();
+
   const addtask = () => {
     if (TaskInput.trim() !== '') {
-      setTaskContainer([...TaskContainer, TaskInput]);
+      setTaskContainer([...TaskContainer, { id: uuidv4(), todo: TaskInput, isCompleted: false }]);
       setTaskInput('');
+
     }
 
   }
-  const [toggle, settoggle] = useState(true);
-  const edit = (iid) => {
-    settoggle(!toggle);
-    const newlist = TaskContainer.map((item, id) => id === iid ? iteminput : item);
+
+  const handledel = (e, newid) => {
+    const newlist = TaskContainer.filter((item) => item.id !== newid);
     setTaskContainer(newlist);
 
+  }
+
+  const handledit = (e, newid) => {
+    const newlist = [...TaskContainer];
+    newlist.map((item) => (
+      item.id === newid ? (setTaskInput(item.todo)
+      ) : null
+    ))
+
+    let newarraylist = TaskContainer.filter((item) => item.id !== newid);
+    setTaskContainer(newarraylist);
 
   }
 
-  const ondel = (iid) => {
-    const newcontainer = TaskContainer.filter((item, id) => id !== iid);
-    setTaskContainer(newcontainer);
-
+  const [kar, setkar] = useState(false);
+  const checkit = (e ,newid) => {
+    const newlist = [...TaskContainer];
+    newlist.map((item) => (
+      item.id === newid ? (
+        setkar(!kar)
+      ) : (null)
+    ))
 
   }
 
-// implemet the check list 
+
 
   return (
     <div className='bg-black text-white flex items-center justify-center min-h-screen text-center '>
       <div>
-        <h1>TODO List</h1>
+        <h1>Todo List</h1>
 
         <div>
           <input type="text" placeholder='Task Here' value={TaskInput} onChange={(e) => setTaskInput(e.target.value)} className='text-black' />
           <button onClick={addtask}>add</button>
         </div>
 
-        {/*container list map  */}
         <div>
-          {TaskContainer.map((item, id) => (
-            <div key={id} className='flex gap-3'>
-              {/* <input type="checkbox" onChange={()=> check(id)} checked={checkkar[id]} name="" id="" /> */}
-              <input type="text" value={item} readOnly={toggle} className={checkkar ? ('text-red-500'): ('text-black')} onChange={(e) => setiteminput(e.target.value)} />
-              <button onClick={() => ondel(id)}>Delete</button>
-              <button onClick={() => edit(id)}>{toggle===false ? 'Save' : 'Edit'}</button>
-            </div>
-          ))}
+          {TaskContainer.map((item) => {
+            return (
+              <div key={item.id} className='flex gap-3'>
+                <input type="checkbox" checked={kar} onChange={(e)=> checkit(e,item.id)}/>
+                <span className={kar ? 'line-through': ''}>{item.todo}</span>
+                <button onClick={(e) => handledit(e, item.id)}>edit</button>
+                <button onClick={(e) => handledel(e, item.id)}>del</button>
+              </div>
+            )
+          })}
         </div>
 
       </div>
